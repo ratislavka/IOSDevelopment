5c90153f0f16aa615b3d586b37234a6f598de1ee
ratislavka <65152341+ratislavka@users.noreply.github.com>
2025-10-22 23:47:08 +0500

    readme + some features

---
+++ RandomApp-lab5/RandomApp-lab5/README.md
@@ -0,0 +1,14 @@
+I chose a casino roulette
+    Spin and win
+    Each spin costs money
+    You have a starting capital
+    When you lost all your money, you can donate more
+
+Tested devices: 
+    Iphone 16
+    Iphone 15 pro
+    Iphone SE3
+    Ipad 10th gen
+
+Limitations:
+    Donated funds dont refill in the app
--- RandomApp-lab5/RandomApp-lab5/ViewController.swift
+++ RandomApp-lab5/RandomApp-lab5/ViewController.swift
@@ -14,18 +14,38 @@ class ViewController: UIViewController {
     @IBOutlet var nameRandomized: UILabel!
     @IBOutlet var resultLabel: UILabel!
     @IBOutlet var userInputTextField: UITextField!
+    @IBOutlet var counterLabel: UILabel!
     
+    var score: Int = 50
+    let costOfSpin: Int = 10
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         resultLabel.isHidden = true
+        userInputTextField.keyboardType = .numberPad
+        counterLabel.text = String(score)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
     }
 
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
     
     @IBAction func randomize(_ sender: Any) {
         randomize()
         
+    }
+    
+    @IBAction func refill(_ sender: Any) {
+        nameRandomized.textColor = .systemRed
+        nameRandomized.text = "Kaspi: 87082414789"
+        imageRandomized.image = .qrCode
+        score += 100
+        counterLabel.text = String(score)
     }
     
     override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
@@ -36,16 +56,32 @@ class ViewController: UIViewController {
     
     
     private func randomize() {
-        resultLabel.isHidden = true
+
         
-        let randomizedValue = rouletteImages.randomElement()
-        imageRandomized.image = randomizedValue?.image
-        nameRandomized.text = "Roulette's value is: \(randomizedValue!.name)"
-        
-        let userGuess = userInputTextField.text
-        
-        if userGuess == randomizedValue?.name {
-            resultLabel.isHidden = false
+        if score - costOfSpin >= 0 {
+            
+            resultLabel.isHidden = true
+            score -= costOfSpin
+            counterLabel.text = String(score)
+            nameRandomized.textColor = .white
+            
+            let randomizedValue = rouletteImages.randomElement()
+            imageRandomized.image = randomizedValue?.image
+            nameRandomized.text = "Roulette's value is: \(randomizedValue!.name)"
+            
+            let userGuess = userInputTextField.text
+            
+            if userGuess == randomizedValue?.name {
+                resultLabel.isHidden = false
+                
+                score += 2 * Int(userGuess!)!
+                counterLabel.text = String(score)
+            }
+        }
+        else{
+            nameRandomized.textColor = .systemRed
+            nameRandomized.text = "You're out of money!"
+            
         }
     }
     
